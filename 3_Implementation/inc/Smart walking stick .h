0dab9b336278ce0b0afc8683fbb44600a460a889
TAMILSELVAN1206 <101853782+TAMILSELVAN1206@users.noreply.github.com>
2022-04-22 12:52:24 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Smart walking stick .h
@@ -0,0 +1,18 @@
+/**
+ * @file Smart walking stick
+ * @author Tamilselvan R P (tamilselvan.18ece@sonatech.ac.in)
+ * @brief Smart walking  stick for the blind
+ * @version 0.1
+ * @date 2022-04-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#include <Servo.h>
+Servo Myservo;
+int pos;
+const int trig=7;
+const int echo=8;
+const int buzzer=11;
